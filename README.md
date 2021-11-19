71b8e94f0c2451b9d872d37269c32ddc4891551f
rangangwei <1074953270@qq.com>
2021-11-19 14:36:18 +0800

    开源规范

--- README.md
+++ README.md
@@ -1,7 +1,187 @@
 # wxcloudrun-springboot
+[![GitHub license](https://img.shields.io/github/license/WeixinCloud/wxcloudrun-express)](https://github.com/WeixinCloud/wxcloudrun-express)
+![GitHub package.json dependency version (prod)](https://img.shields.io/badge/maven-3.6.0-green)
+![GitHub package.json dependency version (prod)](https://img.shields.io/badge/jdk-11-green)
+
+## 简介
+微信云托管 springboot 框架模版，使用springboot框架创建简单的http服务。通过示例创建一张todo_list表，并对其进行增删改查的操作，对应POST/DELETE/PUT/GET四种请求的实现。
+
+![](https://qcloudimg.tencent-cloud.cn/raw/3159427f92e66f3bd431c21e25f18793.png)
+
+
+## 详细介绍：
+1. 一键部署时将默认开通微信云托管中的 MySQL，并自动将数据库基本信息传入了环境变量中，可直接使用。（数据库信息获取及配置详情见:[application.yml](https://github.com/WeixinCloud/wxcloudrun-springboot/blob/main/src/main/resources/application.yml)）
+2. container.config.json 仅用于在微信云托管中创建流水线时配套使用。
+  * 如果不使用流水线，而是用本项目的代码在微信云托管控制台手动「新建版本」，则 container.config.json 配置文件不生效。最终版本部署效果以「新建版本」窗口中手动填写的值为准。
+  * dataBaseName 和 executeSQLs 两个字段只有在服务第一次部署时生效，后续流水线触发的版本更新不会执行（避免重复初始化数据库）。
+
+
+## 快速开始
+前往 [微信云托管快速开始页面](https://developers.weixin.qq.com/miniprogram/dev/wxcloudrun/src/basic/guide.html)，选择相应语言的模板，根据引导完成部署。
+
+
+## 目录结构
+~~~
+.
+├── Dockerfile                      Dockerfile 文件
+├── LICENSE                         LICENSE 文件
+├── README.md                       README 文件
+├── container.config.json           微信云托管流水线配置
+├── mvnw                            mvnw 文件，处理mevan版本兼容问题
+├── mvnw.cmd                        mvnw.cmd 文件，处理mevan版本兼容问题
+├── pom.xml                         pom.xml文件
+├── springboot-cloudbaserun.iml     项目配置文件
+└── src                             源码目录
+    └── main                        源码主目录
+        ├── java                    业务逻辑目录
+        └── resources               资源文件目录
+~~~
+
+## 示例API列表
+
+1 查询所有 todo 项
+
+* URL路径：
+  ```/api/todos```
+  
+* 请求示例：
+```
+curl -X GET  http://{ip}:{port}/api/todos
+```
+
+* 响应示例：
+```
+{
+  "code": 0,
+  "errorMsg": "",
+  "data": [{
+    "id": 1,
+    "title": "工作1",
+    "status": "准备中",
+    "create_time": "2021-11-09T08:45:40Z",
+    "update_time": "2021-11-09T08:45:40Z"
+  }, {
+    "id": 2,
+    "title": "工作2",
+    "status": "已开始",
+    "create_time": "2021-11-09T08:46:11Z",
+    "update_time": "2021-11-09T08:46:11Z"
+  }]
+}
+```
+
+
+2 根据 ID 查询 todo 项
+
+* URL路径：
+  ```/api/todos/:id```
+  
+* 请求示例：
+```
+curl -X GET  http://{ip}:{port}/api/todos/1
+```
+
+* 响应示例：
+```
+{
+  "code": 0,
+  "errorMsg": "",
+  "data": {
+    "id": 1,
+    "title": "工作1",
+    "status": "准备中",
+    "create_time": "2021-11-09T08:45:40Z",
+    "update_time": "2021-11-09T08:45:40Z"
+  }
+}
+```
+
+
+3 新增 todo 项目
+
+* URL路径：
+  ```/api/todos```
+  
+* 请求示例：
+```
+curl http://{ip}:{port}/api/todos \
+  -X POST \
+  -H 'Content-Type: application/json' \
+  -d '{  
+    "title":"工作1",
+    "status":"准备中"
+  }'
+```
+
+* 响应示例：
+```
+{
+  "code": 0,
+  "errorMsg": "",
+  "data": {
+    "id": 1,
+    "title": "工作1",
+    "status": "准备中",
+    "create_time": "2021-11-09T08:45:40Z",
+    "update_time": "2021-11-09T08:45:40Z"
+  }
+}
+```
+
+4 根据 ID 修改 todo 项目
+
+* URL路径：
+  ```/api/todos```
+  
+* 请求示例：
+```
+curl http://{ip}:{port}/api/todos \
+  -X PUT \
+  -H 'Content-Type: application/json' \
+  -d '{  
+    "id":1,
+    "status":"已完成"
+  }'
+```
+
+* 响应示例：
+```
+{
+  "code": 0,
+  "errorMsg": ""
+}
+```
+
+5 根据 ID 删除 todo 项
+
+* URL路径：
+  ```/api/todos/:id```
+  
+* 请求示例：
+```
+curl http://{ip}:{port}/api/todos/1 \
+  -X DELETE \
+  -H 'Content-Type: application/json' \
+  -d '{   }'
+```
+
+* 响应示例：
+```
+{
+  "code": 0,
+  "errorMsg": ""
+}
+```
+
+
+
+
+
+
+
 微信云托管 springboot框架构建HTTP服务端示例
 
-简介：Spring Boot(https://spring.io/projects/spring-boot) 是 Pivotal 团队在 Spring 的基础上提供的一套全新的开源框架，其目的是为了简化 Spring 应用的搭建和开发过程。了解在微信云托管上如何用springboot框架创建简单的http服务。通过示例创建一张todo_list表，并对其进行增删改查的操作，对应POST/DELETE/PUT/GET四种请求的实现。
+简介：Spring Boot(https://spring.io/projects/spring-boot) 是 Pivotal 团队在 Spring 的基础上提供的一套全新的开源框架，其目的是为了简化 Spring 应用的搭建和开发过程。了解在微信云托管上如何
 
 版本： JDK11(修改版本，需要同步修改[Dockerfile](https://github.com/WeixinCloud/wxcloudrun-springboot/blob/main/Dockerfile)中的基础镜像)
 
--- src/main/java/com/tencent/wxcloudrun/controller/ToDoController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/ToDoController.java
@@ -1,5 +1,7 @@
 package com.tencent.wxcloudrun.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.tencent.wxcloudrun.config.ApiResponse;
 import com.tencent.wxcloudrun.dto.CreateToDoRequest;
 import com.tencent.wxcloudrun.dto.UpdateToDoRequest;
@@ -30,18 +32,11 @@ import java.util.List;
 public class ToDoController {
 
   final ToDoService todoService;
+  final Logger logger;
 
   public ToDoController(@Autowired ToDoService todoService) {
     this.todoService = todoService;
-  }
-
-  /**
-   * 主页页面
-   * @return API response html
-   */
-  @GetMapping
-  public String index() {
-    return "<!doctype html><html lang=\"en\"><head><meta charset=\"utf-8\"/><link rel=\"icon\" href=\"https://cloudbase-run-todolist-92bb28a0d-1258016615.tcloudbaseapp.com/favicon.ico\"/><meta name=\"viewport\" content=\"width=device-width,initial-scale=1\"/><meta name=\"theme-color\" content=\"#000000\"/><meta name=\"description\" content=\"Web site created using create-react-app\"/><link rel=\"apple-touch-icon\" href=\"https://cloudbase-run-todolist-92bb28a0d-1258016615.tcloudbaseapp.com/logo192.png\"/><link rel=\"manifest\" href=\"https://cloudbase-run-todolist-92bb28a0d-1258016615.tcloudbaseapp.com/manifest.json\"/><title>Todo List</title><link href=\"https://cloudbase-run-todolist-92bb28a0d-1258016615.tcloudbaseapp.com/static/css/2.20aa2d7b.chunk.css\" rel=\"stylesheet\"><link href=\"https://cloudbase-run-todolist-92bb28a0d-1258016615.tcloudbaseapp.com/static/css/main.d8680f04.chunk.css\" rel=\"stylesheet\"></head><body><noscript>You need to enable JavaScript to run this app.</noscript><div id=\"root\"></div><script>!function(e){function t(t){for(var n,l,a=t[0],p=t[1],i=t[2],f=0,s=[];f<a.length;f++)l=a[f],Object.prototype.hasOwnProperty.call(o,l)&&o[l]&&s.push(o[l][0]),o[l]=0;for(n in p)Object.prototype.hasOwnProperty.call(p,n)&&(e[n]=p[n]);for(c&&c(t);s.length;)s.shift()();return u.push.apply(u,i||[]),r()}function r(){for(var e,t=0;t<u.length;t++){for(var r=u[t],n=!0,a=1;a<r.length;a++){var p=r[a];0!==o[p]&&(n=!1)}n&&(u.splice(t--,1),e=l(l.s=r[0]))}return e}var n={},o={1:0},u=[];function l(t){if(n[t])return n[t].exports;var r=n[t]={i:t,l:!1,exports:{}};return e[t].call(r.exports,r,r.exports,l),r.l=!0,r.exports}l.m=e,l.c=n,l.d=function(e,t,r){l.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},l.r=function(e){\"undefined\"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:\"Module\"}),Object.defineProperty(e,\"__esModule\",{value:!0})},l.t=function(e,t){if(1&t&&(e=l(e)),8&t)return e;if(4&t&&\"object\"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(l.r(r),Object.defineProperty(r,\"default\",{enumerable:!0,value:e}),2&t&&\"string\"!=typeof e)for(var n in e)l.d(r,n,function(t){return e[t]}.bind(null,n));return r},l.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return l.d(t,\"a\",t),t},l.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},l.p=\"https://cloudbase-run-todolist-92bb28a0d-1258016615.tcloudbaseapp.com/\";var a=this.webpackJsonptodo=this.webpackJsonptodo||[],p=a.push.bind(a);a.push=t,a=a.slice();for(var i=0;i<a.length;i++)t(a[i]);var c=p;r()}([])</script><script src=\"https://cloudbase-run-todolist-92bb28a0d-1258016615.tcloudbaseapp.com/static/js/2.18b41bed.chunk.js\"></script><script src=\"https://cloudbase-run-todolist-92bb28a0d-1258016615.tcloudbaseapp.com/static/js/main.bde3e603.chunk.js\"></script></body></html>";
+    this.logger = LoggerFactory.getLogger(ToDoController.class);
   }
 
   /**
@@ -50,6 +45,7 @@ public class ToDoController {
    */
   @GetMapping(value = "/api/todos")
   ApiResponse getAll() {
+    logger.info("/api/todos get request");
     Optional<List<ToDo>> todoList = todoService.getAll();
     if (todoList.isPresent()) {
       return ApiResponse.ok(todoList);
@@ -67,7 +63,7 @@ public class ToDoController {
    */
   @PostMapping(value = "/api/todos")
   ApiResponse create(@RequestBody CreateToDoRequest request) {
-
+    logger.info("/api/todos post request");
     ToDo todo = new ToDo();
     todo.setTitle(request.getTitle());
     todo.setStatus(request.getStatus());
@@ -84,6 +80,7 @@ public class ToDoController {
    */
   @DeleteMapping(value = "/api/todos/{id}")
   ApiResponse delete(@PathVariable Integer id) {
+    logger.info("/api/todos/{id} delete request");
     Optional<ToDo> todo = todoService.getById(id);
     if (!todo.isPresent()) {
       ApiResponse rsp = ApiResponse.error("todo not exist");
@@ -100,6 +97,7 @@ public class ToDoController {
    */
   @GetMapping(value = "/api/todos/{id}")
   ApiResponse get(@PathVariable Integer id) {
+    logger.info("/api/todos/{id} get request");
     Optional<ToDo> todo = todoService.getById(id);
     if (todo.isPresent()) {
       return ApiResponse.ok(todo);
@@ -117,6 +115,7 @@ public class ToDoController {
    */
   @PutMapping(value = "/api/todos")
   ApiResponse update(@RequestBody UpdateToDoRequest request) {
+    logger.info("/api/todos put request");
     Optional<ToDo> queryToDo = todoService.getById(request.getId());
     if (!queryToDo.isPresent()) {
       ApiResponse rsp = ApiResponse.error("todo not exist");
