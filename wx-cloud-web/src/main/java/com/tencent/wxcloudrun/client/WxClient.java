a21c99ae6ea57cb845e3b214a3a8ee03aa7300e9
tangsh <tangsh>
2022-10-30 01:58:25 +0800

    fix:add cloud

--- wx-cloud-web/src/main/java/com/tencent/wxcloudrun/client/WxClient.java
+++ wx-cloud-web/src/main/java/com/tencent/wxcloudrun/client/WxClient.java
@@ -1,6 +1,7 @@
 package com.tencent.wxcloudrun.client;
 
 import com.alibaba.fastjson2.JSON;
+import com.github.kevinsawicki.http.HttpRequest;
 import com.github.wxpay.sdk.WXPayUtil;
 import com.tencent.wxcloudrun.model.request.WxPrePayParam;
 import com.tencent.wxcloudrun.utils.HttpUtils;
@@ -34,33 +35,38 @@ public class WxClient {
 
 
     public void prePay(WxPrePayParam param) {
-        Map<String, String> map = new HashMap<>();
-        map.put("appid", WX_APP_ID);
-        //微信支付分配的商户号
-        map.put("mch_id", WX_MERCHANT_ID);
-        //随机字符串，长度要求在32位以内
-        map.put("nonce_str", WXPayUtil.generateNonceStr());
-        //商品简单描述
-        map.put("body", param.getBody());
-        //商户系统内部订单号
-        map.put("out_trade_no", param.getOut_trade_no());
-        //订单总金额，单位为分
-        map.put("total_fee", param.getTotal_fee());
-        //支持IPV4和IPV6两种格式的IP地址。调用微信支付API的机器IP
-        map.put("spbill_create_ip", IPUtils.getLocalIp());
-        //交易类型  小程序用JSAPI
-        map.put("trade_type", "JSAPI");
-        //回调地址  异步接收微信支付结果通知的回调地址，通知url必须为外网可访问的url，不能携带参数
-        map.put("notify_url", WX_V3_PAY_WEB_HOOK_URL);
-        //trade_type=JSAPI，此参数必传，用户在商户appid下的唯一标识
-        map.put("openid", param.getOpenid());
+        Map<String, String> map = (Map) JSON.parseObject(JSON.toJSONString(param));
+//        map.put("appid", WX_APP_ID);
+//        //微信支付分配的商户号
+//        map.put("mch_id", WX_MERCHANT_ID);
+//        //随机字符串，长度要求在32位以内
+//        map.put("nonce_str", WXPayUtil.generateNonceStr());
+//        //商品简单描述
+//        map.put("body", param.getBody());
+//        //商户系统内部订单号
+//        map.put("out_trade_no", param.getOut_trade_no());
+//        //订单总金额，单位为分
+//        map.put("total_fee", param.getTotal_fee());
+//        //支持IPV4和IPV6两种格式的IP地址。调用微信支付API的机器IP
+//        map.put("spbill_create_ip", IPUtils.getLocalIp());
+//        //交易类型  小程序用JSAPI
+//        map.put("trade_type", "JSAPI");
+//        //回调地址  异步接收微信支付结果通知的回调地址，通知url必须为外网可访问的url，不能携带参数
+//        map.put("notify_url", WX_V3_PAY_WEB_HOOK_URL);
+//        //trade_type=JSAPI，此参数必传，用户在商户appid下的唯一标识
+//        map.put("openid", param.getOpenid());
         try {
             String sign = SignUtils.getSign(map, WX_V3_API_SECRET);// 生成签名 PAY_API_SECRET=微信支付相关API调用时使用的秘钥
             map.put("sign", sign);  // 参数配置 我
-            String xml = WXPayUtil.mapToXml(map);
+            //String xml = WXPayUtil.mapToXml(map);
             //请求微信统一下单接口
-            String xmlStr = HttpUtils.httpRequest(WX_MCH_PRE_ORDER_URL, "POST", xml);
-            log.info("{},{}", JSON.toJSONString(map), JSON.toJSONString(xmlStr));
+            String response = HttpRequest.post(WX_CLOUD_PRE_ORDER_URL)
+                    .header(HttpRequest.HEADER_CONTENT_TYPE, HttpRequest.CONTENT_TYPE_JSON)
+                    .send(JSON.toJSONString(map))
+                    .body();
+            log.info("{},{}", JSON.toJSONString(map), response);
+            //String xmlStr = HttpUtils.httpRequest(WX_MCH_PRE_ORDER_URL, "POST", xml);
+            // log.info("{},{}", JSON.toJSONString(map), JSON.toJSONString(response));
         } catch (Exception e) {
             log.error("请求微信下单接口异常", e);
             throw new RuntimeException("请求微信下单接口异常", e);
--- wx-cloud-web/src/main/java/com/tencent/wxcloudrun/service/impl/AdsInfoServiceImpl.java
+++ wx-cloud-web/src/main/java/com/tencent/wxcloudrun/service/impl/AdsInfoServiceImpl.java
@@ -1,9 +1,13 @@
 package com.tencent.wxcloudrun.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson2.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.github.wxpay.sdk.WXPayUtil;
 import com.tencent.wxcloudrun.client.WxClient;
+import com.tencent.wxcloudrun.model.dto.WxPrePayDTO;
 import com.tencent.wxcloudrun.model.request.AdsPageParam;
 import com.tencent.wxcloudrun.model.dto.PageDTO;
 import com.tencent.wxcloudrun.model.request.WxPrePayParam;
@@ -11,11 +15,13 @@ import com.tencent.wxcloudrun.entity.AdsInfoEntity;
 import com.tencent.wxcloudrun.repository.AdsInfoRepository;
 import com.tencent.wxcloudrun.service.AdsInfoService;
 import com.tencent.wxcloudrun.utils.PageUtils;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author tangsh
@@ -51,5 +57,14 @@ public class AdsInfoServiceImpl implements AdsInfoService {
     @Override
     public void prePay(WxPrePayParam param) {
         wxClient.prePay(param);
+    }
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        String msg ="{\"nonce_str\":\"89f1e03f32174781b43b9472a34819ec\",\"out_trade_no\":\"2021WERUN16478406876373\",\"openid\":\"oXPeb4gGUWuARbGkIcvrb3PXTb30\",\"appid\":\"wx180962a99caf9ff5\",\"total_fee\":\"1\",\"sign\":\"F1416A07F0BE84ED3E22F3DA5BF00D5A\",\"trade_type\":\"JSAPI\",\"mch_id\":\"1633796573\",\"body\":\"测试wechat-pay\",\"notify_url\":\"https://springboot-mvyj-15312-5-1314693576.sh.run.tcloudbase.com/webhook/v1/pay\",\"spbill_create_ip\":\"10.28.16.126\"}";
+//        WxPrePayDTO dto = JSON.parseObject(msg, WxPrePayDTO.class);
+        Map map = JSONObject.parseObject(msg);
+        String xml = WXPayUtil.mapToXml(map);
+        System.out.println(xml);
     }
 }
