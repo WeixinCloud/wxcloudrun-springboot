fd259ed8a47faab9bf675bfa94516e2c1baba14f
sharkdoodoo <shardoodoo@foxmail.com>
2023-08-24 23:11:41 +0800

    feat: add chat rpc

--- src/main/java/com/tencent/wxcloudrun/controller/ChatController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/ChatController.java
@@ -1,16 +1,55 @@
 package com.tencent.wxcloudrun.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tencent.wxcloudrun.dto.ChatRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.UUID;
 
 @RestController
+@Slf4j
 public class ChatController {
 
     @PostMapping(value = "/chat")
-    public String chat(@RequestParam(required = false) String user) {
-        return "Server: "+ UUID.randomUUID().toString();
+    public String chat(@RequestBody ChatRequest request) {
+
+        // 创建RestTemplate对象
+        RestTemplate restTemplate = new RestTemplate();
+
+        // 设置请求头
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", request.getToken());
+
+        // 创建请求体
+        String jsonBody = "{ \"model\": \"gpt-3.5-turbo\", \"messages\": [{\"role\": \"user\", \"content\": \"" + request.getUser() + "\"}], \"temperature\": 0.7 }";
+
+        // 创建HTTP请求实体
+        HttpEntity<String> entity = new HttpEntity<>(jsonBody, headers);
+
+        // 发送POST请求
+        ResponseEntity<String> response = restTemplate.exchange("http://43.134.34.78/chat", HttpMethod.POST, entity, String.class);
+
+        // 打印响应结果
+        log.info("Response status code: {}, body: {}", response.getStatusCode(), response.getBody());
+
+        // 解析JSON响应
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode root = objectMapper.readTree(response.getBody());
+            JsonNode contentNode = root.at("/choices/0/message/content");
+            String content = contentNode.asText();
+            return "Server Reply: " + content;
+        } catch (Exception e) {
+            log.error("error", e);
+        }
+        return "Server Reply: Hello GPT!";
     }
 }
---
+++ src/main/java/com/tencent/wxcloudrun/dto/ChatRequest.java
@@ -0,0 +1,9 @@
+package com.tencent.wxcloudrun.dto;
+
+import lombok.Data;
+
+@Data
+public class ChatRequest {
+    private String token;
+    private String user;
+}
