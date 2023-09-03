95a42a915dc96936108c465c936c5b749bdd4aad
sharkdoodoo <shardoodoo@foxmail.com>
2023-09-03 22:03:54 +0800

    feat: add wxlogin

--- src/main/java/com/tencent/wxcloudrun/WxCloudRunApplication.java
+++ src/main/java/com/tencent/wxcloudrun/WxCloudRunApplication.java
@@ -3,10 +3,17 @@ package com.tencent.wxcloudrun;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.tencent.wxcloudrun.dao"})
-public class WxCloudRunApplication {  
+public class WxCloudRunApplication {
+
+  @Bean
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(WxCloudRunApplication.class, args);
---
+++ src/main/java/com/tencent/wxcloudrun/dto/LoginResponse.java
@@ -0,0 +1,8 @@
+package com.tencent.wxcloudrun.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginResponse {
+    private String openid;
+}
